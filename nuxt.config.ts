2762ea2ad5dee937287a8dd678132284c5628e77
Wanessa <wanessa.araujo.torres@gmail.com>
2026-05-22 17:26:47 +0100

    manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,24 @@ export default defineNuxtConfig({
 
   pwa: {
     manifest: {
-      name: 'Meu App',
-      short_name: 'MeuApp',
+      name: 'Viagem',
+      short_name: 'Viagem',
       theme_color: '#ffffff',
       background_color: '#ffffff',
-      display: 'standalone'
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
     }
   }
 })
